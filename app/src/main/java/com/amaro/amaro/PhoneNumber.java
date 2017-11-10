137808bc9c4fd16bb89d87df116af604f07a4b64
nihal369 <nihalismailk@gmail.com>
2017-11-10 20:16:39 +0530

    OTP layout set

--- app/src/main/java/com/amaro/amaro/PhoneNumber.java
+++ app/src/main/java/com/amaro/amaro/PhoneNumber.java
@@ -2,12 +2,27 @@ package com.amaro.amaro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PhoneNumber extends AppCompatActivity {
+
+    TextView phoneNumberText;
+    EditText phoneNumberForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_number);
+        phoneNumberText=findViewById(R.id.phoneNumberTextView);
+        phoneNumberForm=findViewById(R.id.phoneNumberEditText);
+    }
+
+    public void goToOtp(View view)
+    {
+        phoneNumberText.setText(R.string.otp);
+        phoneNumberForm.setText("");
+        phoneNumberForm.setHint(R.string.otp_hint);
     }
 }

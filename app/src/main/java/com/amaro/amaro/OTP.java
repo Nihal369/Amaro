b8a2c386839c1e9b25c588d4f5031a0f205bc5f7
nihal369 <nihalismailk@gmail.com>
2017-11-14 18:23:28 +0530

    Minor fixes

--- app/src/main/java/com/amaro/amaro/OTP.java
+++ app/src/main/java/com/amaro/amaro/OTP.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -15,22 +14,21 @@ import com.google.firebase.auth.PhoneAuthProvider;
 public class OTP extends AppCompatActivity {
 
     //Object decelerations
-    private static final String TAG = "OTP";
     private String mVerificationId;
     private PhoneAuthProvider.ForceResendingToken mResendToken;
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
-    private FirebaseAuth mAuth;
+
 
     Intent intent;
     String OTPNumber,sentNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
 
-        //Get firebaseAuth Instance
-        mAuth = FirebaseAuth.getInstance();
 
         sentNumber="";
 
@@ -75,9 +73,6 @@ public class OTP extends AppCompatActivity {
     {
         EditText editText=findViewById(R.id.OTPEditText);
         OTPNumber=editText.getText().toString();
-
-        //Get the credential
-        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId,OTPNumber);
 
         //If the sms code matches the user entered text
         if(OTPNumber.equals(sentNumber)) {

e6d83d1d15ebd11ee883557f14a79c0cbd556bcf
nihal369 <nihalismailk@gmail.com>
2017-11-14 16:52:02 +0530

    Code refactoring 1

--- app/src/main/java/com/amaro/amaro/Appoinments.java
+++ app/src/main/java/com/amaro/amaro/Appoinments.java
@@ -3,7 +3,6 @@ package com.amaro.amaro;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -13,20 +12,13 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
 import org.apache.commons.lang3.StringUtils;
-
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -35,43 +27,42 @@ import java.util.Map;
 
 public class Appoinments extends AppCompatActivity {
 
-    //Old section
+    //Activity objects
     ImageView profilePic;
     TextView fullNameTextView, appointmentTextView;
     String fullName,email,phoneNumber;
     Uri profilePicUrl;
-    FirebaseAuth firebaseAuth;
-    FirebaseAuth.AuthStateListener mAuthListener;
 
-    //Firebase section
+    //Firebase objects
     static DatabaseReference mRootRef,userRef,nameRef;
     ArrayList<AppointmentQueue> listOfAppointments;
     Map<String, AppointmentQueue> fireBaseMap;
     ArrayList<String> appointmentTexts;
 
-    //Dynamic layout
+    //Dynamic layout objects
     LinearLayout verticalLayout;
-    TextView textView;
-
-    boolean flag=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appoinments);
 
+        //Retrieve data from LocalDB
         fullName=LocalDB.getFullName();
         email=LocalDB.getEmail();
         phoneNumber=LocalDB.getEmail();
         profilePicUrl=LocalDB.getProfilePicUri();
 
-
+        //View declerations
         profilePic=findViewById(R.id.appoinmentProfilePic);
         fullNameTextView=findViewById(R.id.userNameTextView);
         appointmentTextView =findViewById(R.id.appoinmentTextView);
         verticalLayout=findViewById(R.id.verticalLayout);
 
+        //Set the name of the user
+        fullNameTextView.setText(fullName);
 
+        //Set the profile pic of the user
         Picasso.with(this).
                 load(profilePicUrl)
                 .placeholder(R.drawable.profpic)
@@ -79,85 +70,86 @@ public class Appoinments extends AppCompatActivity {
                 .transform(new CircleTransform())
                 .into(profilePic);
 
-        fullNameTextView.setText(fullName);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        //Refresh the appointment data
         getAppointmentFromFirebase();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        //Set the dynamic layout as blank
         verticalLayout.removeAllViews();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Set the dynamic layout as blank
+        verticalLayout.removeAllViews();
+    }
+
+
+
+    //************Static Fucntions****************************/
+
+    //Static function that writes Appointments to Firebase realtime database
     public static void writeAppointmentToFirebase(String doctorName, String dayOfMonth, String monthName, String time, String year)
     {
-        mRootRef= FirebaseDatabase.getInstance().getReference();
-        userRef=mRootRef.child("Users");
-        nameRef=userRef.child(LocalDB.getFullName());
+        getDatabaseReference();
+
+        //Each appointment of user is referred with a key
         String appointmentId = nameRef.push().getKey();
+
+        //Create a new AppointmentQueue Object
         AppointmentQueue appointment=new AppointmentQueue(doctorName,dayOfMonth,monthName,time,year);
+
+        //Add a new appointment to database
         nameRef.child(appointmentId).setValue(appointment);
     }
 
+    private static void getDatabaseReference()
+    {
+        //Get the database reference
+        mRootRef= FirebaseDatabase.getInstance().getReference();
+        //Get the node reference
+        userRef=mRootRef.child("Users");
+        //Users->username
+        nameRef=userRef.child(LocalDB.getFullName());
+    }
+
+    //************Static Functions****************************/
+
+
+    //Function that retrieve data from the database
     void getAppointmentFromFirebase()
     {
-        mRootRef= FirebaseDatabase.getInstance().getReference();
-        userRef=mRootRef.child("Users");
-        nameRef=userRef.child(LocalDB.getFullName());
+        getDatabaseReference();
+        //Appointment Queue Object to store AppointmentQueue Objects
         listOfAppointments=new ArrayList<>();
+
         nameRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                //Erase the past data and sync realtime
                 listOfAppointments.clear();
+
+                //Map that stores retrived data from the DataSnapshot
                 fireBaseMap = (HashMap<String,AppointmentQueue>) dataSnapshot.getValue();
-                for(String key:fireBaseMap.keySet())
-                {
-                    listOfAppointments.add(fireBaseMap.get(key));
-                }
-                Log.i("NIHAL",listOfAppointments.toString());
-                Iterator iterator=listOfAppointments.iterator();
-                int index=0;
-                appointmentTexts=new ArrayList<String>();
-                while (iterator.hasNext()) {
-                    //APPLY REGEX AND PASS IT TO APPOINTMENTS.CLASS
-                    String str = iterator.next().toString();
-                    String stringToPass="";
-                    stringToPass=stringToPass + StringUtils.substringBetween(str,"dayOfMonth=",",");
-                    stringToPass=stringToPass + " " + StringUtils.substringBetween(str,"{monthName=",",");
-                    stringToPass=stringToPass +" | "+StringUtils.substringBetween(str,"time=",",");
-                    stringToPass=stringToPass +" | "+StringUtils.substringBetween(str,"doctorName=",",");
-                    appointmentTexts.add(stringToPass);
-                }
-                Collections.sort(appointmentTexts);
-                for(String s:appointmentTexts)
-                {
-                    CardView cardView=new CardView(Appoinments.this);
-                    LinearLayout.LayoutParams lp=(new LinearLayout .LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                            150));
-                    lp.setMargins(18,20,18,20);
-                    cardView.setLayoutParams(lp);
-                    cardView.setCardBackgroundColor(0xff313445);
-                    cardView.setPadding(30,30,30,30);
-                    TextView textView = new TextView(Appoinments.this);
-                    LinearLayout.LayoutParams layoutParams=(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.MATCH_PARENT));
-                    textView.setLayoutParams(layoutParams);
-                    textView.setGravity(Gravity.CENTER);
-                    textView.setText(s);
-                    textView.setTextColor(0xffffffff); // hex color 0xAARRGGBB
-                    textView.setTextSize(16);
-                    textView.setTypeface(Typeface.create("monospace", Typeface.NORMAL));
-                    cardView.addView(textView);
-                    verticalLayout.addView(cardView);
-                    index++;
-                }
-                String numberOfAppointments=getString(R.string.appointment)+index;
-                appointmentTextView.setText(numberOfAppointments);
+
+                //Retrieve each value of the map
+                retrieveDataFromFirebaseMap();
+
+               //Get all appointment texts
+                getAppointmentTexts();
+
+                //Set appointment text
+                setAppointmentTexts();
             }
 
             @Override
@@ -168,6 +160,77 @@ public class Appoinments extends AppCompatActivity {
     }
 
 
+    private void retrieveDataFromFirebaseMap()
+    {
+        //Retrive data from the snapshot map
+        for(String key:fireBaseMap.keySet())
+        {
+            listOfAppointments.add(fireBaseMap.get(key));
+        }
+    }
+
+    private void getAppointmentTexts()
+    {
+        //Convert the AppointmentQueue objects to String
+        //Looping through all
+        Iterator iterator=listOfAppointments.iterator();
+
+
+        //AppointmentQueue objects as strings are stored here
+        appointmentTexts=new ArrayList<String>();
+
+        //Convert each appointment to a string format
+        while (iterator.hasNext()) {
+            String str = iterator.next().toString();
+            String stringToPass="";
+            stringToPass=stringToPass + StringUtils.substringBetween(str,"dayOfMonth=",",");
+            stringToPass=stringToPass + " " + StringUtils.substringBetween(str,"{monthName=",",");
+            stringToPass=stringToPass +" | "+StringUtils.substringBetween(str,"time=",",");
+            stringToPass=stringToPass +" | "+StringUtils.substringBetween(str,"doctorName=",",");
+            appointmentTexts.add(stringToPass);
+        }
+
+        //Sort the appointment texts
+        Collections.sort(appointmentTexts);
+    }
+
+    private void setAppointmentTexts()
+    {
+        //Set the dynamic layout with appointment texts
+
+        for(String s:appointmentTexts)
+        {
+            //Card view of the dynamic layout
+            CardView cardView=new CardView(Appoinments.this);
+            LinearLayout.LayoutParams lp=(new LinearLayout .LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    150));
+            lp.setMargins(18,20,18,20);
+            cardView.setLayoutParams(lp);
+            cardView.setCardBackgroundColor(0xff313445);
+            cardView.setPadding(30,30,30,30);
+
+            //Text view of the dynamic layout
+            TextView textView = new TextView(Appoinments.this);
+            LinearLayout.LayoutParams layoutParams=(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT));
+            textView.setLayoutParams(layoutParams);
+            textView.setGravity(Gravity.CENTER);
+            textView.setText(s);
+            textView.setTextColor(0xffffffff); // hex color 0xAARRGGBB
+            textView.setTextSize(16);
+            textView.setTypeface(Typeface.create("monospace", Typeface.NORMAL));
+
+            //Add the text view to card view
+            cardView.addView(textView);
+
+            //Add the card view to linear layout
+            verticalLayout.addView(cardView);
+        }
+
+        //Set the number of appointments to a text view
+        String numberOfAppointments=getString(R.string.appointment)+String.valueOf(listOfAppointments.size());
+        appointmentTextView.setText(numberOfAppointments);
+    }
 
     public void moveToDoctorList(View v)
    {
--- app/src/main/java/com/amaro/amaro/GoogleSignIn.java
+++ app/src/main/java/com/amaro/amaro/GoogleSignIn.java
@@ -1,22 +1,17 @@
 package com.amaro.amaro;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -26,10 +21,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.net.URL;
-import java.sql.Date;
 
 public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
@@ -40,12 +31,13 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
     FirebaseAuth.AuthStateListener mAuthListener;
 
 
-    static String fullName,phoneNumber,email;
-    static Uri profilePicUrl;
+    static String phoneNumber;
+
 
     @Override
     protected void onStart() {
         super.onStart();
+        //Add a listener to check for a google account during app start
         firebaseAuth.addAuthStateListener(mAuthListener);
     }
 
@@ -54,20 +46,28 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_in);
 
+        //Decelerations
         googleSignInButton = findViewById(R.id.googleSignInButton);
         FirebaseApp.initializeApp(this);
+
+        //Check if a user has previously logged in using their google account
         firebaseAuth=FirebaseAuth.getInstance();
         mAuthListener=new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //If there exists a signed in user
                 if(firebaseAuth.getCurrentUser() != null)
                 {
                     //User has already logged in,Jump to Appointment activity
                     FirebaseUser user=firebaseAuth.getCurrentUser();
+
+                    //Assign user details to LocalDB
                     LocalDB.setFullName(user.getDisplayName());
                     LocalDB.setEmail(user.getEmail());
                     LocalDB.setPhoneNumber(user.getPhoneNumber());
                     LocalDB.setProfilePicUri(user.getPhotoUrl());
+
+                    //Jump to appointment activity
                     Intent intent=new Intent(GoogleSignIn.this,Appoinments.class);
                     startActivity(intent);
                     finish();
@@ -75,6 +75,8 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
             }
         };
 
+
+        //OnClick listener of the google sign in button
         googleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -100,13 +102,15 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
                 .build();
     }
 
+
+    //SignIn Function
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
 
-
+    //Result handler of the sign in function
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -116,6 +120,7 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if(result.isSuccess())
             {
+                //Sign into google account
                 GoogleSignInAccount account=result.getSignInAccount();
                 firebaseAuthWithGoogle(account);
             }
@@ -126,6 +131,7 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
         }
     }
 
+    //Link the google account to firebase
     private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
         firebaseAuth.signInWithCredential(credential)
@@ -136,16 +142,19 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient.O
                             // Sign in success, update UI with the signed-in user's information;
                             FirebaseUser user = firebaseAuth.getCurrentUser();
 
+                            //Retrieve google account data and store it in LocalDB
                             assert user != null;
                             LocalDB.setFullName(user.getDisplayName());
                             LocalDB.setEmail(user.getEmail());
                             LocalDB.setPhoneNumber(user.getPhoneNumber());
                             LocalDB.setProfilePicUri(user.getPhotoUrl());
 
+                            //Continue user registration
                             Intent menuIntent = new Intent(GoogleSignIn.this, UserDetails.class);
                             startActivity(menuIntent);
                             finish();
-                        } else {
+                        }
+                        else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(GoogleSignIn.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
